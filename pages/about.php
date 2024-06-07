5d6b3cbfe0b2c46a937f368cfc19052e4b31a425
Bayu S <khususpby@gmail.com>
2024-06-07 23:14:35 +0700

    feat: about, home list sedikit

---
+++ pages/about.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="./style/about.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inika:wght@400;700&family=Inria+Serif:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Inter:wght@100..900&family=Italiana&family=Krona+One&family=Syncopate:wght@400;700&family=Tenor+Sans&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="navbar">
+        <ul class="menu">
+            <li><a href="">Home</a></li>
+            <li><a href="./pages/about.php">About</a></li>
+            <li class="logo"><a>infongopi.</a></li>
+            <li><a href="./pages/cafe.php">List</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <div class="auth-button">
+            <a href="./pages/register.php" class="button">Register</a>
+            <a href="./pages/login.php" class="button">Login</a>
+        </div>
+    </div>
+    <div class="content">
+        <div class="title">
+            <h1>ABOUT US</h1>
+        </div>
+        <div class="about-1">
+            <img src="../public/assets/about-1.png" alt="about-1">
+            <p>Kami menyediakan informasi coffee shop di surabaya timur bosqqqqqqqqqqqqq</p>
+        </div>
+        <div class="informasi-container">
+            <div class="informasi">
+            <p>Informasi Lengkap</p>
+            <p>tiap coffee shop di surabaya timur.</p>
+            </div>
+        </div>
+        <div class="about-2">
+            <img src="../public/assets/about-2.png" alt="about-1">
+            <p>Anda tidak perlu bingung mencari informasi coffee shop yang kadang tidak tentu. Kami memberikan informasi terbaru dan terlengkap!</p>
+        </div>
+        <div class="tim-container">
+            <h1>Tim Kami</h1>
+            <div class="tim">
+                <div class="row-1">
+                    <div>
+                        <p>Bayu Setiawan</p>
+                        <p>Khususpby@gmail.com</p>
+                    </div>
+                    <div>
+                        <p>Muhammad Hilmy Aziz</p>
+                        <p>Aziz@gmail.com</p>
+                    </div>
+                </div>
+                <div class="row-1">
+                    <div class="raja">
+                        <p>Rajawali Shaktika Anugrah Prasetya</p>
+                        <p>Khususpby@gmail.com</p>
+                    </div>
+                    <div class="bagus">
+                        <p>Bagus Satrio Wicaksono</p>
+                        <p>Aziz@gmail.com</p>
+                    </div>
+                </div>
+                <div class="row-1">
+                    <div class="akbar">
+                        <p>Muhammad Akbar Nurdianto</p>
+                        <p>Khususpby@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+            <p>Copyright infongopi.</p>
+        </div>
+</body>
+</html>
--- pages/cafe.php
+++ pages/cafe.php
@@ -12,25 +12,44 @@ $result = $conn->query($cafe);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/cafe.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inika:wght@400;700&family=Inria+Serif:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Inter:wght@100..900&family=Italiana&family=Krona+One&display=swap" rel="stylesheet">
     <title>Cafe</title>
 </head>
 <body>
-    <h1>List Cafe</h1>
+<div class="navbar">
+        <ul class="menu">
+            <li><a href="../index.html">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li class="logo"><a href="#">infongopi.</a></li>
+            <li><a href="#">List</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <img class="profile" src="../public/assets/profile-icon.svg" alt="profile">
+    </div>
+    <div class="search-container">
+        <div class="search">
+            <img src="../public/assets/search-icon.svg" alt="seach-icon">
+            <input type="text" placeholder="Masukkan lokasi daerah anda">
+        </div>
+    </div>
     <div class="cafe-container">
         <div class="info">
         <?php
-            if ($result->num_rows > 0) {
-                while($row = $result->fetch_assoc()) {
-                    echo "<p> Nama : " . $row["nama"] . "</p>";
-                    echo "<p> Deskripsi : " . $row["deskripsi"] . "</p>";
-                    echo "<p> Pro : " . $row["pro"] . "</p>";
-                    echo "<p> Cons : " . $row["cons"] . "</p>";
-                    echo "<p> Lokasi : " . $row["lokasi"] . "</p>";
-                }
-            } else {
-                echo "<p>Tidak ada cafe<p>";
-            }
-            ?>
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "<p> Nama : " . $row["nama"] . "</p>";
+        echo "<p> Deskripsi : " . $row["deskripsi"] . "</p>";
+        echo "<p> Pro : " . $row["pro"] . "</p>";
+        echo "<p> Cons : " . $row["cons"] . "</p>";
+        echo "<p> Lokasi : " . $row["lokasi"] . "</p>";
+    }
+} else {
+    echo "<p>Tidak ada cafe<p>";
+}
+?>
         </div>
     </div>
 </body>
